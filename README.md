3770ad345b4b227f6cec9647fb9092af348a5a18
thdud__ <sylim1114y@gmail.com>
2024-05-13 12:06:25 +0900

    admin 원위, models __str__()

--- README.md
+++ README.md
@@ -17,5 +17,9 @@
    1. models
       1. Character
          1. name, feature, created_at, updated_at
+         2. `__str__()` : 객체를 출력할 때 알맞은 string으로 출력하자
       2. python manage.py makemigrations 콩순이
       3. python manage.py migrate 콩순이
+   2. admin
+      1. Character
+      2. python manage.py createsuperuser
--- 원위/models.py
+++ 원위/models.py
@@ -7,3 +7,6 @@ class Character(models.Model) :
     feature = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True) #데이터가 만들어질 때 자동으로 날짜, 시간 기록
     updated_at = models.DateTimeField(auto_now=True)    #데이터 수정 시 자동으로 날짜, 시간을 기록
+
+    def __str__(self):  #클래스를 출력할 때 이 함수를 호출하여 문자화한다
+        return f'이름 : {self.name}, 특징 : {self.feature}, 만든날짜 : {self.created_at}, 수정날짜 : {self.updated_at}'
